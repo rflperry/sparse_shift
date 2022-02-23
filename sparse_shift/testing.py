dc19e0343bad1c738bec8ce8d100693c4de198e3
rflperry <rflperry@gmail.com>
2022-02-23 11:42:53 +0100

    update numshifts

--- sparse_shift/testing.py
+++ sparse_shift/testing.py
@@ -7,7 +7,7 @@ from hyppo.ksample import MMD
 from sparse_shift import KCD
 
 
-def test_structure(Xs, parent_graph, reps=1000, n_jobs=None):
+def test_mechanism_shifts(Xs, parent_graph, reps=1000, n_jobs=None, alpha=0.05):
     """
     Tests pairwise mechanism equality
     
@@ -29,23 +29,25 @@ def test_structure(Xs, parent_graph, reps=1000, n_jobs=None):
     parent_graph = np.asarray(parent_graph)
     M = parent_graph.shape[0]
     pvalues = np.ones((M, E, E))
-    
+
     for e1 in range(E):
-        for e2 in range(e1+1, E):
+        for e2 in range(e1 + 1, E):
             for m in range(M):
                 parents = np.where(parent_graph[m] != 0)[0]
                 if len(parents) == 0:
                     stat, pvalue = MMD().test(
-                        Xs[e1][:, m], Xs[e2][:, m],
-                        reps=reps, workers=n_jobs)
+                        Xs[e1][:, m], Xs[e2][:, m], reps=reps, workers=n_jobs
+                    )
                 else:
                     stat, pvalue = KCD(n_jobs=n_jobs).test(
                         np.vstack((Xs[e1][:, parents], Xs[e2][:, parents])),
                         np.concatenate((Xs[e1][:, m], Xs[e2][:, m])),
-                        np.asarray([0]*Xs[e1].shape[0] + [1]*Xs[e2].shape[0]),
+                        np.asarray([0] * Xs[e1].shape[0] + [1] * Xs[e2].shape[0]),
                         reps=reps,
                     )
                 pvalues[m, e1, e2] = pvalue
                 pvalues[m, e2, e1] = pvalue
-                
-    return pvalues
+
+    num_shifts = np.sum(pvalues <= alpha) // 2
+
+    return num_shifts, pvalues
